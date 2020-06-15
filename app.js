442add1506854142195b21bc1ff6ccee61c947b4
Saurav Singh <itzsrv@outlook.com>
2020-06-16 05:23:03 +0530

    feat: add js ui-controller

--- app.js
+++ app.js
@@ -53,3 +53,29 @@ var budgetController = (function(){
 
 })();
 
+
+//UI CONTROLLER
+var UIController = (function(){
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    return{
+        getInput: function(){
+
+            return {
+                 type : document.querySelector(DOMstrings.inputType).value,
+                 description : document.querySelector(DOMstrings.inputDescription).value,
+                 value : document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
+})();
+
+
