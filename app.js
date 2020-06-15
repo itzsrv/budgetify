dba0fff5e966d90fdf261aa954aadca4b4d69124
Saurav Singh <itzsrv@outlook.com>
2020-06-16 05:23:31 +0530

    feat: add js global-controller

--- app.js
+++ app.js
@@ -79,3 +79,35 @@ var UIController = (function(){
 })();
 
 
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl,UICtrl){
+
+    var setupEventListeners = function(){
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress',function(event){
+            if(event.keyCode === 13 || event.which === 13){
+                ctrlAddItem();
+            }
+        });
+    };
+   
+    var ctrlAddItem = function(){
+        //TO DO
+        var input = UICtrl.getInput();
+
+        budgetCtrl.addItem(input.type, input.description, input.value);
+        
+    };
+
+    return {
+        init: function(){
+            console.log('Application has started');
+            setupEventListeners();
+        }
+    };
+
+})(budgetController,UIController);
+
+controller.init();
