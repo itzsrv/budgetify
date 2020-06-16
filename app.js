05c92c57114521f0e98596ce596081452c989897
Saurav Singh <itzsrv@outlook.com>
2020-06-16 07:39:49 +0530

    fix: fix #1

--- app.js
+++ app.js
@@ -92,6 +92,18 @@ var UIController = (function(){
             document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
         },
 
+        clearFields: function(){
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            });
+        },
+
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -122,6 +134,7 @@ var controller = (function(budgetCtrl,UICtrl){
 
         UICtrl.addListItem(newItem, input.type);
 
+        UICtrl.clearFields();
         
     };
 
