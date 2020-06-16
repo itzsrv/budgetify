389af31b47e09d8484fcc7763cd60266193b42f8
Saurav Singh <itzsrv@outlook.com>
2020-06-16 07:26:51 +0530

    Revert "fix: issue #1"
    This reverts commit f8246c619dbf0fa1a7317d57cf30f4fe60b2f9be.

--- app.js
+++ app.js
@@ -92,16 +92,6 @@ var UIController = (function(){
             document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
         },
 
-        clearFields: function(){
-            var fields, fieldsArr;
-            document.querySelectorAll(DOMstrings.inputDescription + ', '+ DOMstrings.inputValue);
-
-            fieldsArr = Array.prototype.slice.call(fields);
-
-            fieldsArr.forEach(function(current, number, array){
-                current.value = "";
-            })
-        },
         getDOMstrings: function(){
             return DOMstrings;
         }
@@ -132,7 +122,6 @@ var controller = (function(budgetCtrl,UICtrl){
 
         UICtrl.addListItem(newItem, input.type);
 
-        UICtrl.clearFields();
         
     };
 
